77d8bb39ef17a39ae32740ff0d6fe246cb07d164
smith <syfhub@hotmail.com>
2020-03-23 18:18:59 +0800

    update

--- make.sh
+++ make.sh
@@ -1,21 +1,4 @@
 
-exclude=("sh" "md")
-
-function make_readme(){
-    echo "# CDN" >> "README.md"
-    echo "" >> "README.md"
-    echo "Github + jsDeliver + PicGo + Imagine" >> "README.md"
-    echo "" >> "README.md"
-    echo "## PicGo" >> "README.md"
-    echo "" >> "README.md"
-    echo "[PicGo下载](https://github.com/Molunerfinn/PicGo/releases)" >> "README.md"
-    echo "" >> "README.md"
-    echo "## Imagine" >> "README.md"
-    echo "" >> "README.md"
-    echo "[Imagine下载](https://github.com/meowtec/Imagine/releases)" >> "README.md"
-    echo "" >> "README.md"
-}
-
 function push_to_github(){
     echo "Let's push repo to github!"
     git add .
@@ -24,33 +7,9 @@ function push_to_github(){
     echo "successfully pushed to github!"
 }
 
-function export_file_path(){
-    jslink="https://cdn.jsdelivr.net/gh/smithcloud/CDN"
-    for file in `ls $1`; do
-        ff=$1"/"$file
-        if [ -d $ff ]; then
-            export_file_path $ff
-        else
-            if echo "${exclude[@]}" | grep -w "$file" &>/dev/null || echo "${exclude[@]}" | grep -w "${file##*.}" &>/dev/null; then
-                echo "${file} not export to reademe!"
-            else
-                echo ${jslink}${ff:1} >> "README.md"
-            fi
-        fi
-    done
-}
+python ./scripts/build_directory_md.py &> DIRECTORY.md
 
-function write_readme(){
-    if [ -e "README.md" ]
-    then
-        rm -rf "README.md"
-    fi
-    make_readme
-    echo "## jsDelivr链接" >> "README.md"
-    echo "" >> "README.md"
-    export_file_path .
-}
-
-write_readme
-echo ""
-push_to_github
+if [[ $1 != "local" ]];then
+    echo "Let's push to github!!"
+    push_to_github
+fi
---
+++ scripts/make.sh
@@ -0,0 +1,1 @@
+python ./scripts/build_directory_md.py &> DIRECTORY.md
